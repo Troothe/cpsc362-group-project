27db08e3103b4af502441154a4438f12c1ed3841
Albert Dang <70127717+adang48@users.noreply.github.com>
2021-06-10 16:14:17 -0700

    Update functions.php

--- PHP/functions.php
+++ PHP/functions.php
@@ -1,5 +1,34 @@
 <?php
 
+function attemptLogin($sql_conn, $email, $password) {
+    //Check the connection
+    if(!$sql_conn) {
+        array_push($_SESSION['ErrorsToShow'], "We are having trouble accessing our database.  Please try again.");
+    } else {
+        $emailAttempt = $email;
+        $pwAttempt = md5($password);
+        
+        $sql = "SELECT * FROM users WHERE (email='$emailAttempt') AND (password='$pwAttempt')";
+
+        //Make query and get result
+        $result = mysqli_query($sql_conn, $sql);
+        
+        if(mysqli_num_rows($result) > 1) {
+            //should not get here. Only possible if multiple accounts with same login credentials
+            array_push($_SESSION['ErrorsToShow'], "Multiple accounts with the same email detected");
+            return false;
+        } else if(mysqli_num_rows($result) == 1) {
+            //found results
+            $_SESSION['user'] = mysqli_fetch_all($result, MYSQLI_ASSOC)[0];
+            header("location: home.php");
+        } else {
+            //no results
+            array_push($_SESSION['ErrorsToShow'], "Invalid email or passord, please try again");
+            return false;
+        }
+    }
+}
+
 function createAccount($sql_conn, $email, $password, $password_confirm) {
     if(empty($email) || empty($password) || empty($password_confirm)) {
         $CreateAccountError = "You are missing required fields";
