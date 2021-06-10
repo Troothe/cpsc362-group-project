008c71776116bc29e39bc10f06bcb6035964253c
Troothe <79177605+Troothe@users.noreply.github.com>
2021-06-10 12:24:14 -0700

    Create functions.php
    This file will contain our PHP functions, including anything authentication/data related

---
+++ PHP/functions.php
@@ -0,0 +1,58 @@
+<?php
+
+function createAccount($sql_conn, $email, $password, $password_confirm) {
+    if(empty($email) || empty($password) || empty($password_confirm)) {
+        $CreateAccountError = "You are missing required fields";
+        return false;
+    } else if($password != $password_confirm) {
+        array_push($_SESSION['ErrorsToShow'], "Your passwords do not match");
+        return false;
+    } else if(strlen($password) < 6) {
+        array_push($_SESSION['ErrorsToShow'], "Your password must be at least six characters long");
+        return false;
+    } else if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        array_push($_SESSION['ErrorsToShow'], "Invalid email address");
+        return false;
+    } else {
+        //Check the connection
+        if(!$sql_conn) {
+            array_push($_SESSION['ErrorsToShow'], "We are having trouble accessing our data.  Please try again.");
+        } else {
+            $email_check = $email;
+            //Check if the email address already exists in the database
+            $sql = "SELECT * FROM users WHERE (email='$email_check')";
+            $result = mysqli_query($sql_conn, $sql);
+
+            if(mysqli_num_rows($result) > 0) {
+                array_push($_SESSION['ErrorsToShow'], "Email address entered is already associated with an account");
+                return false;
+            } else {
+                //Create the account record
+                $email = mysqli_real_escape_string($sql_conn, $email);
+                $password = mysqli_real_escape_string($sql_conn, $password);
+
+                $sql = "INSERT INTO users(email, password) VALUES ('$email', MD5('$password'))";
+
+                if(mysqli_query($sql_conn, $sql)) {
+                    //success
+                    $pwAttempt = md5($password);
+                    $emailAttempt = $email;
+                    $sql = "SELECT * FROM users WHERE (email='$emailAttempt') AND (password='$pwAttempt')";
+                    $result = mysqli_query($sql_conn, $sql);
+                    if(mysqli_num_rows($result) == 1) {
+                        $_SESSION['user'] = mysqli_fetch_all($result, MYSQLI_ASSOC)[0];
+                        header("location: home.php");
+                    } else {
+                        array_push($_SESSION['ErrorsToShow'], "Unable to add account.");
+                        return false;
+                    }
+                } else {
+                    array_push($_SESSION['ErrorsToShow'], "Unable to add account.");
+                    return false;
+                }
+            }
+        }
+    }
+}
+
+?>
